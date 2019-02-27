6b9116c60714b7d347534b78cebf846e545d3918
spencer-bos <43038626+spencer-bos@users.noreply.github.com>
2019-02-27 15:17:23 -0500

    Update xrebel_agent.rb

--- lib/java_buildpack/framework/xrebel_agent.rb
+++ lib/java_buildpack/framework/xrebel_agent.rb
@@ -37,7 +37,7 @@ module JavaBuildpack
       def release
         @droplet
           .java_opts
-          .add_javaagent(@droplet.sandbox + 'xrebel.jar')
+          .add_javaagent(@droplet.sandbox + 'jrebel.jar')
       end
 
       protected
